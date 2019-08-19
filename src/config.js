32a1ab986dd815e219b3e9e12986840d84654a6f
Rhys Bowen Harries <rbharries@gmail.com>
2019-08-19 13:10:27 +0100

    login progress

---
+++ src/config.js
@@ -0,0 +1,15 @@
+export default {
+    MAX_ATTACHMENT_SIZE: 5000000,
+    s3: {
+      REGION: "eu-west-2",
+      BUCKET: "notes-app-api"
+    },
+
+    cognito: {
+      REGION: "eu-west-2",
+      USER_POOL_ID: "eu-west-2_pixmDnCYE",
+      APP_CLIENT_ID: "58oq4seqq4jijugsbp6gbc90cu",
+      IDENTITY_POOL_ID: "eu-west-2:735b2a0c-0b86-4532-8169-8fa8be929e1a"
+    }
+  };
+  
