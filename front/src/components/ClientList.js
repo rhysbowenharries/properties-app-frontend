fb8a50f78d65f6863afb5df7656b138246042d91
Rhys Bowen Harries <rbharries@gmail.com>
2019-08-21 22:20:28 +0100

    client list

--- front/src/components/ClientList.js
+++ front/src/components/ClientList.js
@@ -5,10 +5,35 @@ class ClientList extends Component {
 
   render(){
 
+    const clientsComponents = this.props.data.map( (clientInfo) => {
+      
+      const enquieries = clientInfo._embedded.enquieries.map((enquieries) => { 
+        return(
+          <div className= "enquieries" key={enquieries.id}>
+            <h3>{enquieries.datePosted}</h3>
+            <h3>{enquieries.property.titleDescription}</h3>
+            <h4>{enquieries.property.address}</h4>
+          </div>
+        )
+
+      })
+      
+      return(
+        <div className="propertyDetail" key={clientInfo.id}>
+          <h3>{clientInfo.name}</h3> 
+          <h3>{clientInfo.email}</h3>
+          {enquieries}
+        </div>
+      ) 
+    })
+
 console.log(this.props.data);
 
     return(
-      <h1>Client List</h1>
+      <div>
+        <h1>Client List</h1>
+        {clientsComponents}
+      </div>
     )
   }
 }
